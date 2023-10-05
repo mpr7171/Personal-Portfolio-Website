0e47f791c3501c3b6f7f9e552f481882b100c30e
Pranav <maddipranavreddy@gmail.com>
2023-10-06 02:20:48 +0530

    90% Complete

--- portfolio-app-react/src/components/Skills.jsx
+++ portfolio-app-react/src/components/Skills.jsx
@@ -15,7 +15,6 @@ import BootStrap from "../assets/bootstrap.png";
 const Skills = () => {
   return (
     <div name="skills" className="w-full h-auto md:h-screen bg-[#0a192f] text-gray-300">
-      {/* Container */}
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div>
           <p className="text-4xl font-bold inline border-b-4 border-pink-600 ">
