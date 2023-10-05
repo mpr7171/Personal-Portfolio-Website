d9a091bc387931443534676b6eaf3267152a4379
Pranav <maddipranavreddy@gmail.com>
2023-10-05 23:06:44 +0530

    Optimised the Home Section code

--- portfolio-app-react/src/components/Home.jsx
+++ portfolio-app-react/src/components/Home.jsx
@@ -13,20 +13,6 @@ const Home = () => {
     flex: 1,
   };
 
-  const taglineStyles = {
-    fontWeight: 700,
-    letterSpacing: "0.8px",
-    padding: "8px 20px",
-    background: "linear-gradient(360deg, #002147, #003575)",
-    border: "1px solid rgba(255, 255, 255, 0.5)",
-    fontSize: "20px",
-    marginBottom: "16px",
-    maxWidth: "300px",
-    marginTop: "20px",
-    color: "#A7C7E7",
-    display: "inline-block",
-  };
-
   const imageContainerStyles = {
     width: "40%",
     height: "55%",
@@ -44,10 +30,11 @@ const Home = () => {
 
   return (
     <div name="home" className="w-full min-h-screen bg-[#0a192f]">
-      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center min-h-screen" style={containerStyles}>
+      <div
+        className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center min-h-screen"
+        style={containerStyles}
+      >
         <div style={leftContentStyles}>
-          {/* <span style={taglineStyles}>Welcome to my Portfolio</span> */}
-
           <div>
             <p className="text-[#ccd6f6]">Hi, my name is</p>
             <h1 className="text-4xl sm:text-7xl font-bold text-[#fff]">
@@ -60,7 +47,9 @@ const Home = () => {
 
           <div>
             <p className="text-[#F5F5F5] py-4 max-w-[700px]">
-              I’m a front-end developer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I’m focused on building responsive web applications.
+              I’m a front-end developer specializing in building (and
+              occasionally designing) exceptional digital experiences.
+              Currently, I’m focused on building responsive web applications.
             </p>
           </div>
 
@@ -79,11 +68,7 @@ const Home = () => {
         </div>
 
         <div style={imageContainerStyles}>
-          <img
-            src={Profile}
-            alt="Profile-Pic"
-            style={imageStyles}
-          />
+          <img src={Profile} alt="Profile-Pic" style={imageStyles} />
         </div>
       </div>
     </div>
