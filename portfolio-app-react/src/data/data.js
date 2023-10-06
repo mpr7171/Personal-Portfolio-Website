f8d893f431898807f5f8e926547c20ae09d980ae
Pranav <maddipranavreddy@gmail.com>
2023-10-07 02:18:15 +0530

    Added the Preview Images for all the Projects

--- portfolio-app-react/src/data/data.js
+++ portfolio-app-react/src/data/data.js
@@ -1,25 +1,26 @@
-import WorkIm from "../assets/projects/workImg.jpeg";
-import RealEst from "../assets/projects/realestate.jpg";
+import VisualisingTool from "../assets/projects/VTool.jpg"
+import DinosaurDino from "../assets/projects/DinosaurDino.jpg"
+import ReelsApp from "../assets/projects/ReelsApp.png"
 
 export const data = [
   {
     id: 1,
     name: "Short Video App",
-    image: WorkIm,
+    image: ReelsApp,
     github: "https://github.com/mpr7171/Short-Video-App",
     live: "",
   },
   {
     id: 2,
     name: "Visualising Analysis Tool",
-    image: RealEst,
+    image: VisualisingTool,
     github: "https://github.com/mpr7171/Visualising-Tool-Analysis",
     live: "",
   },
   {
     id: 3,
     name: "Dinosaur Dino Game",
-    image: WorkIm,
+    image: DinosaurDino,
     github: "https://github.com/mpr7171/Dinosaur_Dino_Game",
     live: "",
   },
