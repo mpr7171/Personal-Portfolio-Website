b507b518d71bf592cc8fc8b263706a19547e51dd
Pranav <maddipranavreddy@gmail.com>
2023-10-06 18:22:08 +0530

    Made the entire website responsive

--- portfolio-app-react/src/components/About.jsx
+++ portfolio-app-react/src/components/About.jsx
@@ -4,10 +4,10 @@ const About = () => {
   return (
     <div
       name="about"
-      className="w-full h-screen bg-[#0a192f] text-gray-300 pt-16"
+      className="w-full min-h-screen bg-[#0a192f] text-gray-300 pt-16"
     >
-      <div className="flex flex-col justify-center items-center w-full h-full">
-        <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
+      <div className="max-w-[1000px] mx-auto p-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-8">
           <div className="sm:text-left pb-8 pl-4">
             <p className="text-4xl font-bold inline border-b-4 border-pink-600">
               About
--- portfolio-app-react/src/components/Skills.jsx
+++ portfolio-app-react/src/components/Skills.jsx
@@ -14,15 +14,18 @@ import BootStrap from "../assets/bootstrap.png";
 
 const Skills = () => {
   return (
-    <div name="skills" className="w-full h-auto md:h-screen bg-[#0a192f] text-gray-300">
-      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+    <div
+      name="skills"
+      className="w-full min-h-screen bg-[#0a192f] text-gray-300"
+    >
+      <div className="max-w-[1000px] mx-auto p-4">
         <div>
           <p className="text-4xl font-bold inline border-b-4 border-pink-600 ">
             Skills
           </p>
         </div>
 
-        <div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-6 gap-4 text-center py-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 text-center py-8">
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
             <img className="w-20 mx-auto" src={HTML} alt="HTML icon" />
             <p className="my-4">HTML</p>
@@ -32,7 +35,11 @@ const Skills = () => {
             <p className="my-4">CSS</p>
           </div>
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={JavaScript} alt="JavaScript icon" />
+            <img
+              className="w-20 mx-auto"
+              src={JavaScript}
+              alt="JavaScript icon"
+            />
             <p className="my-4">JavaScript</p>
           </div>
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
@@ -64,11 +71,19 @@ const Skills = () => {
             <p className="my-4">SQL</p>
           </div>
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={MaterialUI} alt="Material UI icon" />
+            <img
+              className="w-20 mx-auto"
+              src={MaterialUI}
+              alt="Material UI icon"
+            />
             <p className="my-4">Material UI</p>
           </div>
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={BootStrap} alt="Bootstrap icon" />
+            <img
+              className="w-20 mx-auto"
+              src={BootStrap}
+              alt="Bootstrap icon"
+            />
             <p className="my-4">Bootstrap</p>
           </div>
         </div>
@@ -78,9 +93,3 @@ const Skills = () => {
 };
 
 export default Skills;
-
-
-
-
-
-
