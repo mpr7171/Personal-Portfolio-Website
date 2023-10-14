87f9defe966a1993c977cd9e7f860ccc34592a68
Pranav Reddy <maddipranavreddy@gmail.com>
2023-10-14 18:13:09 +0530

    Adjusted the spacing between Experience and Projects Section

--- portfolio-app-react/src/components/Projects.jsx
+++ portfolio-app-react/src/components/Projects.jsx
@@ -5,9 +5,9 @@ const Work = () => {
   const project = data;
 
   return (
-    <div name="work" className="w-full md:h-screen text-gray-300 bg-[#0a192f]">
+    <div name="work" className="w-full text-gray-300 bg-[#0a192f] pt-14">
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
-        <div className="pb-8">
+        <div className="pb-8 sm:pt-0 sm:pb-4">
           <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">
             Projects
           </p>
@@ -19,14 +19,18 @@ const Work = () => {
               key={index}
               style={{ backgroundImage: `url(${item.image})` }}
               className="shadow-lg shadow-[#040c16] group container rounded-md 
-              flex justify-center text-center items-center mx-auto content-div "
+              flex justify-center text-center items-center mx-auto content-div"
             >
-              <div className="opacity-0 group-hover:opacity-100 ">
-                <span className="text-2xl font bold text-white tracking-wider ">
+              <div className="opacity-0 group-hover:opacity-100">
+                <span className="text-2xl font bold text-white tracking-wider">
                   {item.name}
                 </span>
-                <div className="pt-8 text-center ">
-                  <a href={item.github} target="_blank">
+                <div className="pt-8 text-center">
+                  <a
+                    href={item.github}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
                     <button
                       className="text-center rounded-lg px-4 py-3 m-2
                        bg-white text-gray-700 font-bold text-lg"
