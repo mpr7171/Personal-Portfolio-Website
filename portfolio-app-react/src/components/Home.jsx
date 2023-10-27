5d39c56329cc889a5ad4bb44a22211aaee6264a8
Pranav Reddy <maddipranavreddy@gmail.com>
2023-10-27 23:03:53 +0530

    Made the Home Section Responsive

--- portfolio-app-react/src/components/Home.jsx
+++ portfolio-app-react/src/components/Home.jsx
@@ -31,10 +31,7 @@ const Home = () => {
 
   return (
     <div name="home" className="w-full min-h-screen bg-[#0a192f]">
-      <div
-        className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center min-h-screen"
-        style={containerStyles}
-      >
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center min-h-screen" style={containerStyles}>
         <div style={leftContentStyles}>
           <div>
             <p className="text-[#ccd6f6]">Hi, my name is</p>
