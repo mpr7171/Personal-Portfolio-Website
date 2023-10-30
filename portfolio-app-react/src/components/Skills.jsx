2f69e96477fc9a62720700f3bb757bf4fc9cafd0
Pranav Reddy <maddipranavreddy@gmail.com>
2023-10-31 00:08:54 +0530

    Removed extra space above the Skills Section for smaller screens

--- portfolio-app-react/src/components/Skills.jsx
+++ portfolio-app-react/src/components/Skills.jsx
@@ -16,7 +16,7 @@ const Skills = () => {
   return (
     <div
       name="skills"
-      className="w-full min-h-screen bg-[#0a192f] text-gray-300"
+      className="w-full min-h-screen bg-[#0a192f] text-gray-300 pt-2"
     >
       <div className="max-w-[1000px] mx-auto p-4">
         <div>
